db2cff1b76e5e25e16e6b0938304dbdcdf8f8072
sgokul1500 <sgokul1500@gmail>
2024-04-03 22:29:40 +0530

    agency wala part half done

---
+++ agencyscript.js
@@ -0,0 +1,194 @@
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+let otpGen = 0;
+
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
+});
+
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("sign-up-mode");
+  });
+});
+
+function moveSlider() {
+  let index = this.dataset.value;
+
+  let currentImage = document.querySelector(`.img-${index}`);
+  images.forEach((img) => img.classList.remove("show"));
+  currentImage.classList.add("show");
+
+  const textSlider = document.querySelector(".text-group");
+  textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+  bullets.forEach((bull) => bull.classList.remove("active"));
+  this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+  bullet.addEventListener("click", moveSlider);
+});
+
+function login() {
+    alert("hi")
+  var username = document.getElementById("agencyname").value;
+  var password = document.getElementById("agencyloginpassword").value;
+
+  if (!username || !password) {
+    alert("Please enter both username and passworddddd");
+  }
+
+  fetch("http://127.0.0.1:5000/login?username="+username+"&password="+password, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json' // Add this header if necessary
+  },
+  credentials: 'include', 
+    // headers: {
+    //   "Content-Type": "application/json",
+    // }
+    // body: JSON.stringify({
+    //   username: username,
+    //   password: password
+      
+    // }),
+  })
+    .then((response) => {
+     
+      if (!response.ok) {
+        alert("Failed to login agency",response);       
+      } else{
+        alert("agency logged in successfully");
+        window.location.href = "/dashboardAgency.html";
+      }
+      
+    })
+    .catch((error) => {
+      alert("Error ",error)
+      console.error("Error:", error);
+    });
+
+    return false;
+ 
+}
+function verify(){
+  var username = document.getElementById("agencysignupname").value;
+  var password = document.getElementById("agencypassword").value;
+  var contact = document.getElementById("agencycontact").value;
+  var expertise = document.getElementById("agencyexpert").value;
+  var address = document.getElementById("agencyaddress").value
+  var otp = document.getElementById("otp").value;
+  //Check otp
+  if(otp == otpGen){
+    alert("OTP verified successfully");
+  }else{
+    alert("OTP Verification failed");
+    window.location.href = "loginUser.html";
+  }
+
+      // Register into users mongodb
+      fetch("http://127.0.0.1:5000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          contact: contact,
+          expertise : expertise,
+          address :address,
+          password : password
+        }),
+      })
+        .then((response) => {
+          if (response.ok) {
+            alert("Agency registered successfully");
+            window.location.href="dashboardAgency.html"
+            // Redirect to another page or perform any other action
+          } else {
+            alert("Failed to register Agency");
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    
+      window.location.href = "loginAgency.html";
+
+      return false;
+}
+
+function register() {
+
+  var contact = document.getElementById("agencycontact").value;
+  document.getElementById("verifyOTPButton").style.display='inline'
+  document.getElementById("enterOTP").style.display='flex'
+  document.getElementById("signUpButton").style.display='none'
+
+
+  otpGen = Math.floor(1000 + Math.random() * 9000);
+  // OTP verification
+  fetch("http://127.0.0.1:5000/send-sms", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      to: contact,
+      otp: otpGen 
+      
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        alert("OTP generated successfully");
+       // window.location.href="dashboardUser.html"
+        // Redirect to another page or perform any other action
+      } else {
+        alert("Failed to generate OTP");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+
+   
+
+
+
+
+  // if (!username || !password) {
+  //   alert("Please enter both username and password");
+  // } else if (username === "gokul" && password === "mango") {
+  //   window.location.href = "dashboardUser.html";
+  // }
+  return false;
+}
+// Otp verification
+// var xhr = new XMLHttpRequest();
+// xhr.open('POST', 'http://localhost:5000/send-sms');
+// xhr.setRequestHeader('Content-Type', 'application/json');
+// xhr.onload = function() {
+//     if (xhr.status === 200) {
+//         alert('SMS sent successfully!');
+//     } else {
+//         alert('Failed to send SMS: ' + xhr.responseText);
+//     }
+// };
+// xhr.send(JSON.stringify({to: contact, body: username}));
+
+
+
+
---
+++ backend/agencyapp.py
@@ -0,0 +1,141 @@
+# app.py
+from flask_cors import CORS
+from flask import Flask, request, jsonify, session
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from flask import Flask, request, jsonify
+from twilio.rest import Client
+
+uri = "mongodb+srv://sgokul1500:5MqHKxmL76NB1qNy@sihcluster.dnpyzbh.mongodb.net/?retryWrites=true&w=majority&appName=SIHCluster"
+client = MongoClient(uri, server_api=ServerApi('1'), tls=True,tlsAllowInvalidCertificates=True)
+db = client['SIHDatabase']
+agency_collection = db["agency"]
+
+app = Flask(__name__)
+app.secret_key = 'abcdef123456'  # Change this to a random string
+#app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
+
+CORS(app, supports_credentials=True)
+
+@app.route('/getUsers',methods=['GET'])
+def getUsers():
+    # Perform the database query and directly convert to JSON array
+    user = agency_collection.find_one({})  # Exclude _id field
+    session['user_id'] = str(user['_id'])
+    session.modified = True
+    return user["username"]
+    
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    username = data['username']
+    password = data['password']
+    contact = data['contact']
+    expertise = data['expertise']
+    address = data['address']
+    print("data is",data)
+
+    # Check if username already exists
+    existing_user = agency_collection.find_one({'username': username})
+    if existing_user:
+        return jsonify({'message': 'Username already exists'}), 400
+
+    # Hash the password before storing in the database
+    hashed_password = generate_password_hash(password)
+
+    # Insert new user into the database
+    new_user = {
+        'username': username,
+        'password': hashed_password,
+        'contact' : contact,
+        'expertise' :expertise,
+        'address' : address
+                }
+    agency_collection.insert_one(new_user)
+
+    return jsonify({'message': 'User registered successfully'}), 201
+
+#otp verification
+
+# Your Twilio account SID and auth token
+account_sid = 'ACc148c8c371fbcc099a39816940037a12'
+auth_token = '7026a2886c30eeac8f1680542f7ea697'
+twilio_phone_number = '+16508305627'
+
+# # Initialize Twilio client
+client = Client(account_sid, auth_token)
+
+# # Endpoint for sending SMS messages
+@app.route('/send-sms', methods=['POST'])
+def send_sms():
+    data = request.get_json()
+    to_number = "+91" + str(data['to'])
+    otp = data['otp']
+
+    try:
+        # Send SMS message
+        message = client.messages.create(
+            body=f"This is your otp {otp}",
+            from_=twilio_phone_number,
+            to=to_number
+        )
+        return jsonify({'success': True, 'message_sid': message.sid}), 200
+    except Exception as e:
+        return jsonify({'success': False, 'error': str(e)}), 500
+
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    #data = request.json
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    # Find the user in the database
+    user = agency_collection.find_one({'username': username})
+    if not user or not check_password_hash(user['password'], password):
+        return jsonify({'message': 'Invalid username or password'}), 401
+
+    # Store the user's ID in the session
+    session['user_id'] = str(user['_id'])
+    session.modified = True
+
+    print(session)
+
+    return jsonify({'message': 'Login successful'}), 200
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    # Remove user ID from session
+    session.pop('user_id', None)
+    return jsonify({'message': 'Logged out successfully'}), 200
+
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    # Check if user is logged in
+    if 'user_id' not in session:
+        return jsonify({'message': 'You are not logged in'}), 401
+
+    # Find user by ID
+    user_id = session['user_id']
+    user = agency_collection.find_one({'_id': ObjectId(user_id)})
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    # Return user profile information
+    return jsonify({
+        'username': user['username'],
+        'contact' :user['contact'],
+        'email':user['email'],
+        'address':user['address']
+        
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
